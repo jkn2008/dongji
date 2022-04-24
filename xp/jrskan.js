6532db81244982469c76b124d968cebae46779cc
冬季 <dada1222@163.com>
2022-04-24 13:58:23 +0000

    update xp/jrskan.js.

--- xp/jrskan.js
+++ xp/jrskan.js
@@ -66,9 +66,9 @@
     //"playUrl": "http://play.sportsteam365.com/play/steam{playUrl}.html",
     //"playUrl": "http://play.sportsteam365.com/play/sm.html?id=35&id2=",
     "playUrl": "http://play.sportsteam365.com/play/{playUrl}",
-    "playUa": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36",
+    //"playUa": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36",
 	 
-    //"playUa": "",
+    "playUa": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36",
 	
     "searchUrl": "http://www.jrskan.com?key={wd}",
     "scVodNode": "//div[@class='play_xg']/li",
